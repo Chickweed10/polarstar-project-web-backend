a4edc62448ac15b86102b008b2ef203d6fd6b855
RIN-1011 <9993hye@naver.com>
2022-12-15 21:59:18 +0900

    컨트롤러 test

--- polarstar/src/main/java/com/chickweed/andriod/polarstar/controller/MainController.java
+++ polarstar/src/main/java/com/chickweed/andriod/polarstar/controller/MainController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
-
     @CrossOrigin("*")
-    @GetMapping("/") //ESL 조회
+    @GetMapping("/")
     @ResponseBody
-    public String index() {
+    public String mainIndex() {
         return "Hello!";
+    }
+    @CrossOrigin("*")
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
     }
 }
